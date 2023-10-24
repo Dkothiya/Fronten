1290492a02ab1e5952e60c956996cf7d109d1481
Dhruvil2626 <dhruvilkothiya07@gmail.com>
2023-10-24 17:11:13 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./Components/Nav";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer";
+import SignUp from "./Components/SignUp";
+import Privatecomponent from "./Components/Privatecomponent";
+import Login from "./Components/Login";
+import AddProduct from "./Components/AddProduct";
+import Productlist from "./Components/Productlist";
+import UpdateProduct from "./Components/UpdateProduct";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Privatecomponent />}>
+            <Route path="/" element={<Productlist />} />
+            <Route path="/add" element={<AddProduct />} />
+            <Route
+              path="/update/:id"
+              element={<UpdateProduct/>}
+            />
+            <Route path="/logout" element={<h1>Logout Components</h1>} />
+            <Route path="/profile" element={<h1>profile Components</h1>} />
+          </Route>
+
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Productlist.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Productlist() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    let result = await fetch("http://localhost:5000/products", {
+      headers: {
+        authorization: JSON.parse(localStorage.getItem("token")),
+      },
+    });
+    result = await result.json();
+    setProducts(result);
+  };
+
+  const deleteProduct = async (id) => {
+    let result = await fetch(`http://localhost:5000/product/${id}`, {
+      method: "Delete",
+    });
+    result = await result.json();
+    if (result) {
+      getProducts();
+    }
+  };
+
+  const searchHandle = async (e) => {
+    // console.warn(e.target.value)
+    let key = e.target.value;
+    if (key) {
+      let result = await fetch(`http://localhost:5000/search/${key}`);
+      result = await result.json();
+      if (result) {
+        setProducts(result);
+      }
+    } else {
+      getProducts();
+    }
+  };
+
+  return (
+    <div className="product-list">
+      <h1>Product list</h1>
+      <input
+        type="text"
+        placeholder="Search Product"
+        className="search-product-box"
+        onChange={searchHandle}
+      />
+      <ul>
+        <li>S.no</li>
+        <li>Name</li>
+        <li>Price</li>
+        <li>Category</li>
+        <li>Company</li>
+        <li>Operation</li>
+      </ul>
+      {products.length > 0 ? (
+        products.map((item, index) => (
+          <ul key={item._id}>
+            <li>{index + 1}</li>
+            <li>{item.name}</li>
+            <li>{item.price}</li>
+            <li>{item.category}</li>
+            <li>{item.company}</li>
+            <li>
+              <button onClick={() => deleteProduct(item._id)}>Delete</button>
+              <Link to={"/update/" + item._id}>Update</Link>
+            </li>
+          </ul>
+        ))
+      ) : (
+        <h1>No Record Found</h1>
+      )}
+    </div>
+  );
+}
